7c94cc73be4039d78b968b9baddf5468741b6389
BugHunter-X <askzenai@gmail.com>
2025-01-09 19:14:05 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,6 @@
+function foo(a, b) {
+  if (a === null || a === undefined || b === null || b === undefined) {
+    return 0; // Return a default value or handle the case appropriately
+  }
+  // ... rest of the function
+}
